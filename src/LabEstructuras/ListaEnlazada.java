15b0458592800bfa577304b4687aee6a8628dc28
Natalia Martínez <80662024+natymartinez04@users.noreply.github.com>
2022-03-21 19:01:42 -0500

    Agregar paquetes, falta verificar

--- src/LabEstructuras/ListaEnlazada.java
+++ src/LabEstructuras/ListaEnlazada.java
@@ -5,6 +5,8 @@
  */
 package LabEstructuras;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author natymartinez04
@@ -13,6 +15,7 @@ public class ListaEnlazada {
     
     NodoLista ptr;
     NodoLista link;
+    int tam;
     
     public ListaEnlazada() {
         this.ptr = null;
@@ -23,13 +26,47 @@ public class ListaEnlazada {
             ptr = new NodoLista(n);
         }else{
             NodoLista p = ptr;
-            System.out.println(n.dato);
             while (p.link != null){
                 p = p.link;
             }
             p.link = new NodoLista(n);
+        } 
+    }
+    
+    public NodoArbol BuscaPaquetePadre(NodoArbol raiz,String datoPadre,String datoHijo){
+        Boolean encontroPadre = false;
+        if (raiz != null){
+            if (raiz.dato.equals(datoPadre)){
+                return raiz;
+            }else{
+                if (raiz.hijos.ptr != null){
+                    NodoLista Hijo = raiz.hijos.ptr;
+                    encontroPadre = revisaHijos(raiz,datoPadre,Hijo);
+                    Hijo = Hijo.link;
+                    if (encontroPadre == false && Hijo.link != null){
+                        Hijo = Hijo.link;
+                        BuscaPaquetePadre(Hijo.nodoArbol,datoPadre,datoHijo);
+                    }else if (encontroPadre==true){
+                        Hijo.nodoArbol.hijos.insert(new NodoArbol(datoHijo));
+                    }
+                }  
+            }    
         }
-        
+        return raiz;
+    }
+    
+    public Boolean revisaHijos(NodoArbol p,String dato,NodoLista hijo){
+        boolean encontroPadre = false;
+        if (p.hijos.ptr != null){
+            if (dato.equals(hijo.nodoArbol.dato)){
+                encontroPadre = true;
+                return encontroPadre;
+            }else{
+                hijo = hijo.link;
+                revisaHijos(p,dato,hijo);
+            }
+        }
+        return encontroPadre;
     }
 
 }
