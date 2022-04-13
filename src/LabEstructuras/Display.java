84941c27238cc85f62aa46727e694cdd957fa0f2
tllach <tllach@uninorte.edu.co>
2022-04-13 15:04:31 -0500

    adding a lost carpet

--- src/LabEstructuras/Display.java
+++ src/LabEstructuras/Display.java
@@ -17,7 +17,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
