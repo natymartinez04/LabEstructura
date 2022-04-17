acdaf321bfc204136e70ea79d26321fe1960466a
Natalia Martínez <80662024+natymartinez04@users.noreply.github.com>
2022-04-17 10:37:21 -0500

    Pantalla Cronograma

--- src/LabEstructuras/LabEstructura.java
+++ src/LabEstructuras/LabEstructura.java
@@ -21,8 +21,9 @@ public class LabEstructura {
      */
     public static void main(String[] args) {
         String proyecto;
+        ListaEnlazada listaEntregables = new ListaEnlazada();
         proyecto = JOptionPane.showInputDialog("Ingresa el nombre del proyecto: ");
-        JFrame display = new Display(1500, 1000, proyecto);
+        JFrame display = new Display(1500, 1000, proyecto,listaEntregables);
         display.setDefaultCloseOperation(EXIT_ON_CLOSE);
         display.setSize(1500, 1000);
         display.setResizable(false);
